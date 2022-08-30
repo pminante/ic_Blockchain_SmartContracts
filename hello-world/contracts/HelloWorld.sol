7e822221daa8e591718f914cf9fbe9229922209a
pminante <pedro.minante1@gmail.com>
2022-08-30 10:50:23 -0300

    fimsc

--- hello-world/contracts/HelloWorld.sol
+++ hello-world/contracts/HelloWorld.sol
@@ -1,7 +1,18 @@
 //SPDX-License-Identifizer: me
 pragma solidity ^0.8.9;
 
+contract helloWorld {
+    event UpdateMessages9(string oldStr, string newStr);
 
-contract helloWorld{
+    string public message;
 
+    constructor(string memory initMessage) {
+        message = initMessage;
+    }
+
+    function update(string memory newMessage) public {
+        string memory oldMsg = message;
+        message = newMessage;
+        emit UpdateMessages9(oldMsg, newMessage);
+    }
 }
