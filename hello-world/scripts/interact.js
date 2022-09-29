310a853996a4233964a7ff3407d73e470e329e4d
pminante <pedro.minante1@gmail.com>
2022-09-29 09:21:28 -0300

    criando SM em versão mais antiga

--- hello-world/scripts/interact.js
+++ hello-world/scripts/interact.js
@@ -1,7 +1,9 @@
+// interact.js
+
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-//const { ethers} = require("hardhat");
+
 const contract = require("../artifacts/contracts/HelloWorld.sol/helloWorld.json");
 
 // provider - Alchemy
